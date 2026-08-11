d4929fe04a00456463d147db926d317d7ed377e9
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-08-11 15:07:00 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,16 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#019EE5" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          href="/exec-73f48906-8ed1-4886-81ea-0c0f949bae9d.png"
+        />
       </Head>
       <body style={{ margin: 0, width: '100%', overflowX: 'hidden' }}>
         <Main />
