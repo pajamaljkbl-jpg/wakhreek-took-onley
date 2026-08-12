a492f928fcd75e23ae4808077172a200f3979cb3
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-08-12 10:51:26 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -15,8 +15,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="theme-color" content="#019EE5" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/icon.svg" />
+        <link rel="icon" type="image/png" href="/exec-73f48906-8ed1-4886-81ea-0c0f949bae9d.png" />
+<link rel="apple-touch-icon" href="/exec-73f48906-8ed1-4886-81ea-0c0f949bae9d.png" />
         <link rel="apple-touch-icon" href="/icon.svg" />
       </Head>
 
