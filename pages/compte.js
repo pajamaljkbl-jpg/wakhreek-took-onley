8afe62fc2b61082eeecbf9dacec1e07cc9e230fb
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-08-13 21:16:44 +0000

    Update compte.js

--- pages/compte.js
+++ pages/compte.js
@@ -14,7 +14,13 @@ export default function Compte() {
   const [acceptTerms, setAcceptTerms] = useState(false);
   const [message, setMessage] = useState('');
   const [busy, setBusy] = useState(false);
+async function forgotPassword() {
+  ...
+}
 
+async function submit(e) {
+  ...
+}
   async function submit(e) {
     e.preventDefault();
     setBusy(true); setMessage('');
